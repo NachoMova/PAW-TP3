ebe37d0560ac21d54f59c7abc654db820084d332
Matías <matu.rocha@gmail.com>
2019-05-02 22:25:51 -0300

    Ej 1: Buscaminas version beta

---
+++ Ejercicio 5/js/slider.js
@@ -0,0 +1,16 @@
+var window = window || {},
+    document = document || {},
+    console = console || {},
+    Slider = Slider || {};
+
+window.onload = function() {
+  Slider.init("slider");
+};
+
+
+Slider = {
+
+  init: function () {
+
+  }
+}
