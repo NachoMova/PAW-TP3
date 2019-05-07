fe58ca11bfee29a803ea9a8835ae70d7edc8ecdf
Matías <matu.rocha@gmail.com>
2019-05-07 20:06:05 -0300

    Estilos Buscaminas, init en Criptograma

---
+++ Ejercicio 3/js/cryptogram.js
@@ -0,0 +1,8 @@
+var window = window || {},
+    document = document || {},
+    console = console || {},
+    Cryptogram = Cryptogram || {};
+
+window.onload = function() {
+  Cryptogram.init("cryptogram");
+};
